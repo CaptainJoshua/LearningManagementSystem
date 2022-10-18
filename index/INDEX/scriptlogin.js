8b1a1f7d3adba35153b18f74183e7da70c49df47
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-10-19 00:32:25 +0800

    Added a simple function that validate ID & PASS

--- index/INDEX/scriptlogin.js
+++ index/INDEX/scriptlogin.js
@@ -1,15 +1,30 @@
-
 var input = document.querySelector('.pswrd');
 var show = document.querySelector('.show');
 show.addEventListener('click', active);
-function active(){
-  if(input.type === "password"){
-    input.type = "text";
-    show.style.color = "#1DA1F2";
-    show.textContent = "HIDE";
-  }else{
-    input.type = "password";
-    show.textContent = "SHOW";
-    show.style.color = "#111";
-  }
+
+function active() {
+    if (input.type === "password") {
+        input.type = "text";
+        show.style.color = "#1DA1F2";
+        show.textContent = "HIDE";
+    } else {
+        input.type = "password";
+        show.textContent = "SHOW";
+        show.style.color = "#111";
+    }
+}
+
+// * Login Form Validation
+function validate() {
+    let uID = document.getElementById("uID").value;
+    let uPass = document.getElementById("uPass").value;
+    if (uID == "admin" && uPass == "admin") {
+        alert("Login successful")
+        return true;
+    } else {
+        alert("Wrong ID or Password")
+        document.getElementById("uID").value = "";
+        document.getElementById("uPass").value = "";
+        return false;
+    }
 }
