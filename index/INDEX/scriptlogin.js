bf6f6b7869a1a2ccb77842d4a7bce5c9128e6c3f
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-10-20 00:12:45 +0800

    test1

--- index/INDEX/scriptlogin.js
+++ index/INDEX/scriptlogin.js
@@ -1,15 +1,17 @@
-
 var input = document.querySelector('.pswrd');
 var show = document.querySelector('.show');
 show.addEventListener('click', active);
-function active(){
-  if(input.type === "password"){
-    input.type = "text";
-    show.style.color = "#1DA1F2";
-    show.textContent = "HIDE";
-  }else{
-    input.type = "password";
-    show.textContent = "SHOW";
-    show.style.color = "#111";
-  }
+
+function active() {
+    if (input.type === "password") {
+        input.type = "text";
+        show.style.color = "#1DA1F2";
+        show.textContent = "HIDE";
+    } else {
+        input.type = "password";
+        show.textContent = "SHOW";
+        show.style.color = "#111";
+    }
 }
+
+console.log("Hello World"); // Test 1 10/20/2022
