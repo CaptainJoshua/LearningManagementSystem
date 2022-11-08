2c75ba19e13b6252173ad1dd732fe27404e35f82
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-11-09 04:19:40 +0800

    trial

--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,23 @@ const router = express.Router()
 
 const { Student } = require('../models/student')
 
+router.post('/login', async(req, res) => {
+    const student = await Student.findOne({ email: req.body.email })
+    if (student == null) {
+        return res.status(400).send('Cannot find student')
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, student.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
+
+
 module.exports = router
+
+// time to get some sleep and try again tomorrow
