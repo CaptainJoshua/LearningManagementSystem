402ce7285b3a3661e3e2a83e510705af9235d125
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-11-10 03:23:16 +0800

    more errors

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { Student } = require('../models/student')
 
 router.post('/login', async(req, res) => {
-    const student = await Student.findOne({ email: req.body.email })
+    const student = await Student.findOne({ username: req.body.username })
     if (student == null) {
         return res.status(400).send('Cannot find student')
     }
@@ -22,4 +22,8 @@ router.post('/login', async(req, res) => {
 
 module.exports = router
 
-// time to get some sleep and try again tomorrow
+// will work on this later on to make it work with the database and the front end of the application
+// Time to sleep now and get some rest for tomorrow and the next day
+// I will be back tomorrow to continue working on this project
+// Good night and sweet dreams
+// 10/10/2022   03:22 AM
