9db410436138bf12b060793eded4d5606b7d8162
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-11-19 02:32:07 +0800

    login using DB is functional (have some errors)

--- app.js
+++ app.js
@@ -1,59 +1,85 @@
 const express = require('express')
-const { ObjectId } = require('mongodb')
-const { connectToDb, getDb } = require('./db')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./models/User')
+const PORT = 5000
+const userController = require('./controllers/authController')
+
+mongoose.connect('mongodb://localhost:27017/usersDB', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        family: 4
+    })
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err))
 
 // init app & middleware
 const app = express()
 
-// db connection
-let db
-connectToDb((err) => {
-    if (!err) {
-        app.listen(4000, () => {
-            console.log('app listening on port 4000')
-        })
-        db = getDb()
-    }
+app.set('view engine', 'ejs')
+app.use('/public', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('index');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+
+app.get('/logout', (req, res) => {
+    res.render('index');
+});
+
+// ****PORTS REQUEST ARE HERE**** //
+app.post('/dashboard', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ username: username }, (err, foundResults) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundResults.password === password) {
+                // res.send('Successfully logged in!');
+                res.render('dashboard', { username: `${username}` });
+
+            } else {
+                res.send('Incorrect username or Password!');
+            }
+        }
+    })
+});
+
+// app.get('/logout', userController.userLogout, (req, res) => {
+
+// })
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
 
-// routes
-app.get('/LMS/students', (req, res) => {
-    let lms = []
+// // routes
+// app.get('/LMS/students', (req, res) => {
+//     let lms = []
 
-    db.collection('student')
-        .find()
-        .sort({ name: 1 })
-        .forEach(book => lms.push(book))
-        .then(() => {
-            res.status(200).json(lms)
-        })
-        .catch(() => {
-            res.status(500).json({ error: 'Could not fetch the documents' })
-        })
-})
-
-
-
-// routes
-app.get('/students', (req, res) => {
-    res.json({ msg: "welcome to the world" })
-})
-
-app.get('/students/:id', (req, res) => {
-    // const id = req.params.id
-    if (ObjectId.isValid(req.params.id)) {
-        db.collection('student')
-            .findOne({ _id: ObjectId(req.params.id) })
-            .then(doc => {
-                res.status(200).json(doc)
-            })
-            .catch(err => {
-                res.status(500).json({ error: 'Could not fetch the document' })
-            })
-    } else {
-        res.status(500).json({ error: 'Not a valid doc id' })
-    }
-
-})
-console.log('test')
-console.log('test')
+//     db.collection('student')
+//         .find()
+//         .sort({ name: 1 })
+//         .forEach(book => lms.push(book))
+//         .then(() => {
+//             res.status(200).json(lms)
+//         })
+//         .catch(() => {
+//             res.status(500).json({ error: 'Could not fetch the documents' })
+//         })
+// })
