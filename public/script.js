e334e2501b889bed19f39773295c64a046265312
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-12-05 17:45:11 +0800

    change file path and new images

---
+++ public/script.js
@@ -0,0 +1,11 @@
+// let select = document.querySelector('#grlvl');
+// let heading = document.getElementById('section');
+
+// select.addEventListener('change', function() {
+//     if (this.value == 'Prep') {
+//         heading.innerHTML = '<input type="text" name="section" id="section" class="form-control" placeholder="Enter a section" value="Joyful" disabled />'.valueOf('Joyful');
+//     } else if (this.value == 'Kinder') {
+//         heading.innerHTML = '<input type="text" name="section" id="section" class="form-control" placeholder="Enter a section" value="Cheerful" disabled/>'.valueOf('Cheerful');
+//     }
+// }); 
+// The line above is working, but it is not saving a data for database. It show an output when certain select value has choose, but it is not saving a data for database.
